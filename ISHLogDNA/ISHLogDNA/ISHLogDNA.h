fae7cc658509412af969e497e5fda8cb8127160d
Tom Baranes <tom.baranes@gmail.com>
2018-07-12 10:04:41 +0200

    Add carthage support

---
+++ ISHLogDNA/ISHLogDNA/ISHLogDNA.h
@@ -0,0 +1,20 @@
+//
+//  ISHLogDNA.h
+//  ISHLogDNA
+//
+//  Created by Tom Baranes on 12/07/2018.
+//  Copyright © 2018 iosphere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISHLogDNA.
+FOUNDATION_EXPORT double ISHLogDNAVersionNumber;
+
+//! Project version string for ISHLogDNA.
+FOUNDATION_EXPORT const unsigned char ISHLogDNAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISHLogDNA/PublicHeader.h>
+
+#import <ISHLogDNA/ISHLogDNAService.h>
+
--- ISHLogDNA/ISHLogDNATests/ISHLogDNAMessageTests.swift
+++ ISHLogDNA/ISHLogDNATests/ISHLogDNAMessageTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import ISHLogDNA
 
 class ISHLogDNAMessageTests: XCTestCase {
 
